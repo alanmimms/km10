20e723251a554120ab747932be6509a53d1ff476
Alan Mimms <alanmimms@gmail.com>
2024-06-05 22:11:39 -0700

    Starting to understand flags from ADD

--- km10-test.cpp
+++ km10-test.cpp
@@ -43,6 +43,17 @@ TEST(ADDInstruction, ADDFlavor) {
 
   EXPECT_EQ(state.AC[5], W36(a.u + b.u));
   EXPECT_EQ(state.memP[0100], b);
+
+  EXPECT_EQ(state.flags.ndv | state.flags.fuf, 0);
+  EXPECT_EQ(state.flags.afi | state.flags.pub, 0);
+  EXPECT_EQ(state.flags.uio | state.flags.usr, 0);
+  EXPECT_EQ(state.flags.fpd | state.flags.fov, 0);
+
+  EXPECT_EQ(state.flags.tr1, 0);
+  EXPECT_EQ(state.flags.tr2, 0);
+  EXPECT_EQ(state.flags.cy1, 0);
+  EXPECT_EQ(state.flags.cy0, 0);
+  EXPECT_EQ(state.flags.ov, 0);
 }
 
 
@@ -65,10 +76,12 @@ int main(int argc, char *argv[]) {
 void Logger::nyi(KMState &state) {
   s << " [not yet implemented]";
   cerr << "Not yet implemented at " << state.pc.fmtVMA() << endl;
+  ADD_FAILURE();
 }
 
 
 void Logger::nsd(KMState &state) {
   s << " [no such device]";
   cerr << "No such device at " << state.pc.fmtVMA() << endl;
+  ADD_FAILURE();
 }
